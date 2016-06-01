993cd3d1d1ec6ac0384322468e40e618221f0396
mukulrawat1986 <mukulsrawat@gmail.com>
2016-06-01 18:10:18 +0530

    a minimal echo server

---
+++ gopl.io/ch1/server1/main.go
@@ -0,0 +1,18 @@
+// server1 is a minimal echo server
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls a handler
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+// handler echoes the Path component of the requesterd url.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path=%q\n", r.URL.Path)
+}
