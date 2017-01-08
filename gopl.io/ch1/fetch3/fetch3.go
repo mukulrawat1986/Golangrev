1d04e1fd4d2248432495f613c9d1a4471a064f3b
Mukul Rawat <mukulsrawat@gmail.com>
2017-01-08 15:39:35 +0530

    fetch3 prints out the content of a url and prints out the status code

---
+++ gopl.io/ch1/fetch3/fetch3.go
@@ -0,0 +1,32 @@
+// fetch3 uses io.Copy(dst, src) instead of ioutil.ReadAll
+// Also print out the status code
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: error while reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("Status code: %s\n", resp.Status)
+	}
+}
