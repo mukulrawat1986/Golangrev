831148605bf0143d306f3570a0411d146d710c59
Mukul Rawat <mukulsrawat@gmail.com>
2017-01-18 22:58:34 +0530

    nonempty returns a slice holding only the nonempty strings.

---
+++ gopl.io/ch4/nonempty/nonempty.go
@@ -0,0 +1,24 @@
+// Nonempty is an example of an in-place slice algorithm
+package main
+
+import "fmt"
+
+func main() {
+	s := []string{"hello", "world", "", "new", "", "hell", "", ""}
+	fmt.Println(s)
+	s = nonempty(s)
+	fmt.Println(s)
+}
+
+// nonempty returns a slice holding only the non-empty strings.
+// The underlying array is modified during the call
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
