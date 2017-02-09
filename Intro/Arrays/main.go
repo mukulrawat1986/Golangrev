05ddcce4d2f007e1cfc88a2220a74b5f3893c4e7
mukulrawat1986 <mukulsrawat@gmail.com>
2017-02-09 15:57:08 +0530

    slicing a slice

--- Intro/Arrays/main.go
+++ Intro/Arrays/main.go
@@ -8,15 +8,10 @@ func printer(w []string) {
 	}
 
 	fmt.Printf("\n")
-
-	w[2] = "blue"
 }
 
 func main() {
 	// slice
 	words := []string{"the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"}
-	fmt.Printf("%s\n", words[2])
-	fmt.Printf("%d\n", len(words))
-	printer(words)
-	printer(words)
+	printer(words[5:])
 }
