b20afbc02dd223667cba1b744d9c9ce43940daaf
mukulrawat1986 <mukulsrawat@gmail.com>
2017-02-11 22:47:41 +0530

    hello world program and importing our package

---
+++ MasteringGo/myfirstapp/helloworld.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mukulrawat1986/Golangrev/MasteringGo/myfirstapp/utility"
+)
+
+func main() {
+	fmt.Println("Hello Go World!")
+	utility.SayHello()
+}
