2c61aa70c4c73c6ea31dcd3c1a06920c420ee179
mukulrawat1986 <mukulsrawat@gmail.com>
2017-02-13 22:02:48 +0530

    anonymous functions, and showing how memory is being shared between calling routine and anonymous go routines

--- MasteringGo/Section3/Section3-3/Section3-3.go
+++ MasteringGo/Section3/Section3-3/Section3-3.go
@@ -9,6 +9,24 @@ func main() {
 	go waitAndSay("World")
 	fmt.Println("Hello")
 	time.Sleep(3 * time.Second)
+
+	go func(s string) {
+		time.Sleep(2 * time.Second)
+		fmt.Println(s)
+	}("World")
+
+	time.Sleep(3 * time.Second)
+
+	word := "Hello"
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		fmt.Println(word)
+	}()
+
+	word = "this is changed now"
+
+	time.Sleep(3 * time.Second)
 }
 
 func waitAndSay(s string) {
