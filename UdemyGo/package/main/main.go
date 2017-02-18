88bc39cdeedaa6660cca80fcc83b78182f464489
mukulrawat1986 <mukulsrawat@gmail.com>
2017-02-18 23:31:39 +0530

    packaged done

---
+++ UdemyGo/package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mukulrawat1986/Golangrev/UdemyGo/package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+	fmt.Println(stringutil.MyName)
+}
