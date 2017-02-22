53ff13d6a77bf23c89620dba86e56dc7bdd113d1
mukulrawat1986 <mukulsrawat@gmail.com>
2017-02-22 17:09:07 +0530

    bit shift operations with iota

--- UdemyGo/constants/iota/main.go
+++ UdemyGo/constants/iota/main.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 const (
-	_ = iota      // 0
-	B = iota * 10 // 1 * 10
-	C = iota * 10 // 2 * 10
+	_  = iota             // 0
+	KB = 1 << (iota * 10) // 1<<(1 * 10)
+	MB = 1 << (iota * 10) // 1<<(2 * 10)
+	GB = 1 << (iota * 10) // 1<<(3 * 10)
+	TB = 1 << (iota * 10) // 1<<(4 * 10)
 )
 
 func main() {
-
-	fmt.Println(B)
-	fmt.Println(C)
+	fmt.Println("binary\t\tdecimal")
+	fmt.Printf("%b\t", KB)
+	fmt.Printf("%d\n", KB)
+	fmt.Printf("%b\t", MB)
+	fmt.Printf("%d\n", MB)
+	fmt.Printf("%b\t", GB)
+	fmt.Printf("%d\n", GB)
+	fmt.Printf("%b\t", TB)
+	fmt.Printf("%d\n", TB)
 }
