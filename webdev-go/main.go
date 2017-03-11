c9616d9804a77ee3b5d44a3adc5b097706edfdd0
mukulrawat1986 <mukulsrawat@gmail.com>
2017-03-11 11:20:41 +0530

    introduction to templates

--- webdev-go/main.go
+++ webdev-go/main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Page struct {
+	Name string
+}
+
 func main() {
 
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Go Web Development")
+
+		p := Page{Name: "Gopher"}
+
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
+
+		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		// fmt.Fprintf(w, "Hello, Go Web Development")
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
