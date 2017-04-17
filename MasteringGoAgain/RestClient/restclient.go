db5f91f87e62e839206b2c1a3b389d815972c8e6
mukulrawat1986 <mukulsrawat@gmail.com>
2017-04-17 22:15:36 +0530

    add better error handling

--- MasteringGoAgain/RestClient/restclient.go
+++ MasteringGoAgain/RestClient/restclient.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
-	resp, err := http.Get("http://api.theysaidso.com/qod")
+	resp, err := http.Get("http://api.theysaidso.com/qod.json")
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Fprintf(os.Stderr, "Error during Get Request: %v\n", err)
+		os.Exit(1)
 	}
 	defer resp.Body.Close()
 	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Fprintf(os.Stderr, "Error during reading content: %v\n", err)
+		os.Exit(1)
 	}
 	fmt.Println(string(content))
 }
