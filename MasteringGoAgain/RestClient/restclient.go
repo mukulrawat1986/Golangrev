8676d659db6249ef063c2f603070950afea5636d
mukulrawat1986 <mukulsrawat@gmail.com>
2017-04-17 22:02:14 +0530

    rest client to get quote of the day

---
+++ MasteringGoAgain/RestClient/restclient.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://api.theysaidso.com/qod")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(content))
+}
