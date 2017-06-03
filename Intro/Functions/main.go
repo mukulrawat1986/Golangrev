98506579070ddef962bc976c835791fe7f1abb15
mukulrawat1986 <mukulsrawat@gmail.com>
2017-06-03 15:10:15 +0530

    another example of variadic function

--- Intro/Functions/main.go
+++ Intro/Functions/main.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func printer(msgs ...string) {
+func printer(format string, msgs ...string) {
 	for _, msg := range msgs {
-		fmt.Printf("%s\n", msg)
+		fmt.Printf(format, msg)
 	}
 }
 
 func main() {
-	printer("Hello", "world", "this", "is", "dog")
+	printer("%s\n", "Hello", "world", "this", "is", "dog")
 }
