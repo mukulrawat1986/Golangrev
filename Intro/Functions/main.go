bb96730271243df6eece0ab47db601c597429c5c
mukulrawat1986 <mukulsrawat@gmail.com>
2017-06-03 15:10:15 +0530

    named return parameters in functions

--- Intro/Functions/main.go
+++ Intro/Functions/main.go
@@ -1,15 +1,10 @@
 package main
 
-import "os"
+import "fmt"
 
-func printer(msg string) error {
-	f, err := os.Create("Helloworld.txt")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	f.Write(msg)
-	return err
+func printer(msg string) (e error) {
+	_, e = fmt.Printf("%s\n", msg)
+	return
 }
 
 func main() {
