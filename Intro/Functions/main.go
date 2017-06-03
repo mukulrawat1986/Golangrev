9f02b42607ef0b758e86dacfa00e5d1648ab52f8
mukulrawat1986 <mukulsrawat@gmail.com>
2017-06-03 10:10:10 +0530

    multiple parameters in a function

--- Intro/Functions/main.go
+++ Intro/Functions/main.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
-func printer(msg string) {
-	fmt.Printf("%s\n", msg)
+func printer(msg, msg2 string, repeat int) {
+	for repeat > 0 {
+		fmt.Printf("%s", msg)
+		fmt.Printf("%s\n", msg2)
+		repeat -= 1
+	}
 }
 
 func main() {
-	printer("Hello, World!")
+	printer("Hello", " World!", 5)
 }
