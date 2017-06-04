7c8e5fe24ef810688762fd2550e67db36ce3d204
mukulrawat1986 <mukulsrawat@gmail.com>
2017-06-04 15:10:15 +0530

    passing an array by value to a function

--- Intro/ArraySlice/main.go
+++ Intro/ArraySlice/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+func printer(w [4]string) {
+	for _, word := range w {
+		fmt.Printf("%s", word)
+	}
+	fmt.Printf("\n")
+}
+
 func main() {
 	// an array of strings
 	words := [4]string{"the", "quick", "brown", "fox"}
-
-	fmt.Printf("%s\n", words[2])
+	printer(words)
 }
