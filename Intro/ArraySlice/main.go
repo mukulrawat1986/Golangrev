a98d5387ba0dcef0d0c829b89a14a3141b24e990
mukulrawat1986 <mukulsrawat@gmail.com>
2017-06-04 15:10:15 +0530

    Array cannot be changed when passed to a function by value

--- Intro/ArraySlice/main.go
+++ Intro/ArraySlice/main.go
@@ -7,10 +7,13 @@ func printer(w [4]string) {
 		fmt.Printf("%s", word)
 	}
 	fmt.Printf("\n")
+
+	w[2] = "blue"
 }
 
 func main() {
 	// an array of strings
 	words := [4]string{"the", "quick", "brown", "fox"}
+	printer(words)
 	printer(words)
 }
