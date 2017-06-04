ad08ae34a85f858cdec3e5fff935c3e015f32885
mukulrawat1986 <mukulsrawat@gmail.com>
2017-06-04 15:10:15 +0530

    an array of fixed size

--- Intro/ArraySlice/main.go
+++ Intro/ArraySlice/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// an array of strings of unknown number of values
-	words := [...]string{"the", "quick", "brown", "fox"}
+	// an array of strings
+	words := [4]string{"the", "quick", "brown", "fox"}
 
 	fmt.Printf("%s\n", words[2])
 }
