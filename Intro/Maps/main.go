842c97f85beac3cbfaafd7fb2b95330a7eca6bff
mukulrawat1986 <mukulsrawat@gmail.com>
2017-06-05 15:10:15 +0530

    declare a map using literals

--- Intro/Maps/main.go
+++ Intro/Maps/main.go
@@ -3,60 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	// create a map
-	dayMonths := make(map[string]int)
-	dayMonths["Jan"] = 31
-	dayMonths["Feb"] = 28
-	dayMonths["Mar"] = 31
-	dayMonths["Apr"] = 30
-	dayMonths["May"] = 31
-	dayMonths["Jun"] = 30
-	dayMonths["Jul"] = 31
-	dayMonths["Aug"] = 31
-	dayMonths["Sep"] = 30
-	dayMonths["Oct"] = 31
-	dayMonths["Nov"] = 30
-	dayMonths["Dec"] = 31
-
-	fmt.Printf("Days in February: %d\n", dayMonths["Feb"])
-
-	// accessing a key that does not exist
-	// it returns the zero value of the corresponding value
-	fmt.Printf("Days in January: %d\n", dayMonths["January"])
-
-	// Distinguishing between error, since key did not exit and the zero value
-	days, ok := dayMonths["Januaray"]
-	if !ok {
-		fmt.Printf("Can't get days for January")
-	} else {
-		fmt.Printf("%d\n", days)
+	// create a map using a literal
+	dayMonths := map[string]int{
+		"Jan": 31,
+		"Feb": 28,
+		"Mar": 31,
+		"Apr": 30,
+		"May": 31,
+		"Jun": 30,
+		"Jul": 31,
+		"Aug": 31,
+		"Sep": 30,
+		"Oct": 31,
+		"Nov": 30,
+		"Dec": 31,
 	}
 
-	// iterating over a map
-	for month, days := range dayMonths {
-		fmt.Printf("%s has %d days\n", month, days)
+	for month, day := range dayMonths {
+		fmt.Printf("%s %d\n", month, day)
 	}
-
-	var has31 int
-
-	for _, days := range dayMonths {
-		if days == 31 {
-			has31++
-		}
-	}
-
-	fmt.Printf("%d months have 31 days\n", has31)
-
-	// delete items from the map
-	delete(dayMonths, "Feb")
-
-	var has28 int
-
-	for _, days := range dayMonths {
-		if days == 28 {
-			has28++
-		}
-	}
-
-	fmt.Printf("%d months have 28 days\n", has28)
 }
