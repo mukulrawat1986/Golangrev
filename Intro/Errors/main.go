8b801ade1cfd08093a9faf0b8cabc5bf1ae80ebf
Mukul Rawat <mukulsrawat@gmail.com>
2017-06-06 10:10:10 +0530

    custom error using Errorf

--- Intro/Errors/main.go
+++ Intro/Errors/main.go
@@ -6,12 +6,16 @@ import (
 )
 
 func printer(msg string) error {
+	if msg == "" {
+		return fmt.Errorf("Unwilling to print empty string")
+	}
 	_, err := fmt.Printf("%s\n", msg)
 	return err
 }
 
 func main() {
 	if err := printer("Hello, World!!"); err != nil {
+		fmt.Printf("Printer failed: %s\n", err)
 		os.Exit(1)
 	}
 }
