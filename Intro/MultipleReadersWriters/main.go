214b6c7667c3fd8f4f21f03d571837870f243220
Mukul Rawat <mukulsrawat@gmail.com>
2017-06-07 19:19:19 +0530

    refactoring code so multiple goroutines receiving on a single channel

--- Intro/MultipleReadersWriters/main.go
+++ Intro/MultipleReadersWriters/main.go
@@ -24,24 +24,28 @@ func getPage(url string) (int, error) {
 	return len(body), nil
 }
 
-func getter(url string, size chan string) {
-	length, err := getPage(url)
-	if err == nil {
-		size <- fmt.Sprintf("%s has length %d", url, length)
+func worker(urlCh chan string, sizeCh chan string) {
+	for {
+		url := <-urlCh
+		length, err := getPage(url)
+		if err == nil {
+			sizeCh <- fmt.Sprintf("%s has length %d", url, length)
+		} else {
+			sizeCh <- fmt.Sprintf("Error getting %s: %s", url, err)
+		}
 	}
 }
 
 func main() {
-	urls := []string{"http://www.google.com/", "http://www.yahoo.com",
-		"http://www.bing.com", "http://bbc.co.uk"}
+	// 	urls := []string{"http://www.google.com/", "http://www.yahoo.com",
+	//		"http://www.bing.com", "http://bbc.co.uk"}
 
-	size := make(chan string)
+	urlCh := make(chan string)
+	sizeCh := make(chan string)
 
-	for _, url := range urls {
-		go getter(url, size)
-	}
+	go worker(urlCh, sizeCh)
 
-	for i := 0; i < len(urls); i++ {
-		fmt.Printf("%s\n", <-size)
-	}
+	urlCh <- "http://www.oreilly.com/"
+
+	fmt.Printf("%s\n", <-sizeCh)
 }
