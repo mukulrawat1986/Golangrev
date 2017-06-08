678cba63c0079391888fd1b77547d09ea7879b71
Mukul Rawat <mukulsrawat@gmail.com>
2017-06-08 10:10:10 +0530

    stopping multiple goroutines at the same time by closing the channel

--- Intro/ClosingChannels/main.go
+++ Intro/ClosingChannels/main.go
@@ -7,24 +7,30 @@ import (
 	"time"
 )
 
-func printer(msg string, goCh chan bool) {
-	<-goCh
-	fmt.Printf("%s\n", msg)
+func printer(msg string, stopCh chan bool) {
+	for {
+		select {
+		case <-stopCh:
+			return
+		default:
+			fmt.Printf("%s\n", msg)
+		}
+	}
 }
 
 func main() {
 
-	goCh := make(chan bool)
+	stopCh := make(chan bool)
 
 	for i := 0; i < 10; i++ {
-		go printer(fmt.Sprintf("printer: %d", i), goCh)
+		go printer(fmt.Sprintf("printer: %d", i), stopCh)
 	}
 
 	time.Sleep(5 * time.Second)
 
 	// by closing the goChannel, all goroutines will get a zero value
 	// from the channel
-	close(goCh)
+	close(stopCh)
 
 	time.Sleep(5 * time.Second)
 
