d30f5eef3c6efd7aed1759c515a028b63ad685fc
Mukul Rawat <mukulsrawat@gmail.com>
2017-06-10 10:10:10 +0530

    a simple package

---
+++ Intro/Shuffler/shuffle.go
@@ -0,0 +1,15 @@
+package Shuffler
+
+import "math/rand"
+
+type Shuffleable interface {
+	Len() int
+	Swap(i, j int)
+}
+
+func Shuffle(s Shuffleable) {
+	for i := 0; i < s.Len(); i++ {
+		j := rand.Intn(s.Len() - i)
+		s.Swap(i, j)
+	}
+}
