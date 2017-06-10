acee03f694587028499509673bc63f442480f92a
Mukul Rawat <mukulsrawat@gmail.com>
2017-06-10 10:10:10 +0530

    switching on type

--- Intro/TypeAssertions/main.go
+++ Intro/TypeAssertions/main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func whatIsThis(i interface{}) {
-	fmt.Printf("%T\n", i)
+	switch v := i.(type) {
+	case string:
+		fmt.Printf("It's a string %s\n", v)
+	case uint32:
+		fmt.Printf("It's an unsigned 32-bit integer %d\n", v)
+	default:
+		fmt.Printf("Don't know %v\n", v)
+	}
 }
 
 func main() {
@@ -11,5 +18,7 @@ func main() {
 	// empty interface
 	// an interface with no methods in it, it satisfied everything
 	// Gives us an unknown type that's determined at run time.
-	whatIsThis(42)
+	whatIsThis(uint32(42))
+	whatIsThis("Hello")
+	whatIsThis([...]string{"A", "B", "C"})
 }
