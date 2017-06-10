f27a3d8151bfa0a583c89a518f97d3bb66566e9a
Mukul Rawat <mukulsrawat@gmail.com>
2017-06-10 10:10:10 +0530

    a simple interface and type that implements it

---
+++ Intro/Interfaces/main.go
@@ -0,0 +1,35 @@
+// Interface
+// Interfaces define behavior rather than specific type
+
+package main
+
+import "math/rand"
+import "fmt"
+
+type shuffler interface {
+	Len() int
+	Swap(i, j int)
+}
+
+func shuffle(s shuffler) {
+	for i := 0; i < s.Len(); i++ {
+		j := rand.Intn(s.Len() - i)
+		s.Swap(i, j)
+	}
+}
+
+type intSlice []int
+
+func (is intSlice) Len() int {
+	return len(is)
+}
+
+func (is intSlice) Swap(i, j int) {
+	is[i], is[j] = is[j], is[i]
+}
+
+func main() {
+	is := intSlice{1, 2, 3, 4, 5, 6}
+	shuffle(is)
+	fmt.Printf("%v\n", is)
+}
