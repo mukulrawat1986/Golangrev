ac53f356dc04ec29644c662e45033c26b45dcc71
Mukul Rawat <mukulsrawat@gmail.com>
2017-06-11 10:10:10 +0530

    add another interface

--- Intro/Shuffler/shuffle.go
+++ Intro/Shuffler/shuffle.go
@@ -7,9 +7,35 @@ type Shuffleable interface {
 	Swap(i, j int)
 }
 
+type WeightedShuffleable interface {
+	Shuffleable
+	Weight(i int) int
+}
+
 func Shuffle(s Shuffleable) {
 	for i := 0; i < s.Len(); i++ {
 		j := rand.Intn(s.Len() - i)
 		s.Swap(i, j)
 	}
+}
+
+func WeightedShuffle(w WeightedShuffleable) {
+	total := 0
+	for i := 0; i < w.Len(); i++ {
+		total += w.Weight(i)
+	}
+
+	for i := 0; i < w.Len(); i++ {
+		pos := rand.Intn(total)
+		cum := 0
+		for j := 0; j < w.Len(); j++ {
+			cum += w.Weight(j)
+			if pos >= cum {
+				total -= w.Weight(j)
+				w.Swap(i, j)
+				break
+			}
+		}
+	}
+
 }
