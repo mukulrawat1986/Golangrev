a6e8a93d2e60ca06fd4dc77567a057cb55b321fd
mukulrawat1986 <mukulsrawat@gmail.com>
2017-06-29 16:19:32 +0530

    Hello World

---
+++ ModernGo/Hello/main.go
@@ -0,0 +1,12 @@
+// Entry package for our program
+package main
+
+import (
+	"fmt"
+)
+
+// entry function in our main package
+func main() {
+	// fmt package is used for formatted i/o
+	fmt.Println("Hello, World!!!")
+}
