e9b4c47733b7bf80fd298b65cfb2784c0a1aebf3
Mukul Rawat <mukulsrawat@gmail.com>
2017-08-01 10:10:10 +0530

    Pointers in go

--- MasteringGoProgramming/general/main.go
+++ MasteringGoProgramming/general/main.go
@@ -15,4 +15,14 @@ func main() {
 	c := []int{99, 44}
 	c = append(c, 82)
 	fmt.Println("Slice c", c, len(c))
+
+	// pointer
+	var p1 *int // declare a pointer
+	i := 4      // create an integer using type inference
+	p1 = &i     // assign the address of the integer to the pointer
+	fmt.Println(p1)
+
+	p2 := &i         // create a pointer via type inference
+	fmt.Println(*p2) // use the * in order to dereference the pointer and get the original value
+
 }
