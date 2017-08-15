2f8ad0238dda88b3a08545c3f2ee717e8987532e
Mukul Rawat <mukulsrawat@gmail.com>
2017-08-15 10:10:10 +0530

    add more logging

--- ModernGo/EloquentGo/Functions/main.go
+++ ModernGo/EloquentGo/Functions/main.go
@@ -10,9 +10,12 @@ func main() {
 
 func compareNumbers(i1, i2 int) (bool, int) {
 	if i1 > i2 {
+		fmt.Println("First number is greather than second number")
 		return false, i1 - i2
 	} else if i2 > i1 {
+		fmt.Println("Second number is greater than first number")
 		return false, i2 - i1
 	}
+	fmt.Println("Numbers are equal!!!")
 	return true, 0
 }
