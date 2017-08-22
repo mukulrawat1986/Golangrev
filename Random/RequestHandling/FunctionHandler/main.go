b0a1eac47a77164636cae3636733e6601bd493e2
Mukul Rawat <mukulsrawat@gmail.com>
2017-08-22 10:10:10 +0530

    create custom handlers using closure

--- Random/RequestHandling/FunctionHandler/main.go
+++ Random/RequestHandling/FunctionHandler/main.go
@@ -6,9 +6,12 @@ import (
 	"log"
 )
 
-func timeHandler(w http.ResponseWriter, r *http.Request) {
-	tm := time.Now().Format(time.RFC1123)
-	w.Write([]byte("The time is: " + tm))
+func timeHandler(format string) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		tm := time.Now().Format(format)
+		w.Write([]byte("The time is: " + tm))
+	}
+	return http.HandlerFunc(fn)
 }
 
 func main() {
@@ -16,8 +19,11 @@ func main() {
 	// create a new servemux
 	mux := http.NewServeMux()
 
+	// create a handler
+	th := timeHandler(time.RFC1123)
+
 	// add it to servemux
-	mux.HandleFunc("/time", timeHandler)
+	mux.HandleFunc("/time", th)
 
 	log.Println("Listening......")
 	log.Fatal(http.ListenAndServe(":3000", mux))
