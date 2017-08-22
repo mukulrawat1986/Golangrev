8114d7f7fc385b96b7d3c8347f04361f58fc599a
Mukul Rawat <mukulsrawat@gmail.com>
2017-08-22 10:10:10 +0530

    another use of having a custom handler

--- Random/RequestHandling/Customhandler/main.go
+++ Random/RequestHandling/Customhandler/main.go
@@ -19,8 +19,11 @@ func main() {
 	// create a new servemux
 	mux := http.NewServeMux()
 
-	th := &timeHandler{format: time.RFC1123}
-	mux.Handle("/time", th)
+	th1123 := &timeHandler{format: time.RFC1123}
+	mux.Handle("/time/rfc1123", th1123)
+
+	th3339 := &timeHandler{format: time.RFC3339}
+	mux.Handle("/time/rfc3339", th3339)
 
 	log.Println("Listening......")
 	log.Fatal(http.ListenAndServe(":3000", mux))
