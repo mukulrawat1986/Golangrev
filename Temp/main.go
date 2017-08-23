38963717ec3e092e6e26d4e8522b60d535641908
Mukul Rawat <mukulsrawat@gmail.com>
2017-08-23 10:10:10 +0530

    refactored and added end to end test

--- Temp/main.go
+++ Temp/main.go
@@ -4,46 +4,20 @@ import (
 	"bufio"
 	"os"
 	"fmt"
-	"strings"
+	"io"
 )
 
 var ActorNames = []string{}
 
-type stringReader interface {
-	ReadString(byte) (string, error)
-}
+func Run(in stringReader, out io.Writer) {
+	AskForNames(in)
 
-func AskForName(r stringReader) {
-	fmt.Println("Please enter an actor's name: ")
-	name, _ := r.ReadString('\n')
-	name = strings.TrimSpace(name)
-	ActorNames = append(ActorNames, name)
-}
-
-func AskForNames(r stringReader) {
-	asking := true
-	for asking {
-		if len(ActorNames) < 2 {
-			AskForName(r)
-		} else {
-			fmt.Println("Would you like to add another name? (y/n)")
-			answer, _ := r.ReadString('\n')
-			answer = strings.ToLower(strings.TrimSpace(answer))
-			switch (answer) {
-			case "y":
-				AskForName(r)
-			case "n":
-				asking = false
-			}
-		}
+	fmt.Fprintf(out, "You selected the following %d actors:\n", len(ActorNames))
+	for _, v := range ActorNames {
+		fmt.Fprintln(out, v)
 	}
 }
 
 func main() {
-	AskForNames(bufio.NewReader(os.Stdin))
-
-	fmt.Printf("You selected the following %d actors: \n", len(ActorNames))
-	for _, v := range ActorNames {
-		fmt.Println(v)
-	}
+	Run(bufio.NewReader(os.Stdin), os.Stdout)
 }
--- Temp/main_test.go
+++ Temp/main_test.go
@@ -3,53 +3,24 @@ package main_test
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
-	"github.com/mukulrawat1986/Golangrev/Temp"
 	"bytes"
+	"github.com/mukulrawat1986/Golangrev/Temp"
 )
 
-func setup() {
-	main.ActorNames = []string{}
-}
-
-func Test_AskForName(t *testing.T) {
+func Test_E2E(t *testing.T) {
 	setup()
-
 	a := assert.New(t)
-	b := []byte("Brad\n")
+
+	b := []byte("Brad Pitt\nAngelina Jolie\nn\n")
 
 	r := bytes.NewBuffer(b)
-	main.AskForName(r)
+	w := &bytes.Buffer{}
 
-	a.Equal(len(main.ActorNames), 1)
-	a.Equal(main.ActorNames[0], "Brad")
-}
+	main.Run(r, w)
 
-func Test_AskForNames(t *testing.T) {
-	setup()
+	res := w.String()
 
-	a := assert.New(t)
-	b := []byte("Brad\nTom\nn\n")
-
-	r := bytes.NewBuffer(b)
-	main.AskForNames(r)
-
-	a.Equal(len(main.ActorNames), 2)
-	a.Equal(main.ActorNames[0], "Brad")
-	a.Equal(main.ActorNames[1], "Tom")
-}
-
-func Test_AskForNames_FourNames(t *testing.T) {
-	setup()
-
-	a := assert.New(t)
-	b := []byte("Brad\nTom\ny\nMary\ny\nSmith\nn\n")
-
-	r := bytes.NewBuffer(b)
-	main.AskForNames(r)
-
-	a.Equal(len(main.ActorNames), 4)
-	a.Equal(main.ActorNames[0], "Brad")
-	a.Equal(main.ActorNames[1], "Tom")
-	a.Equal(main.ActorNames[2], "Mary")
-	a.Equal(main.ActorNames[3], "Smith")
+	a.Contains(res, "You selected the following 2 actors:")
+	a.Contains(res, "Brad Pitt")
+	a.Contains(res, "Angelina Jolie")
 }
