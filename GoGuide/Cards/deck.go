9d39665f926771c9a005666fbf8fdbd83be5abe3
Mukul Rawat <mukulsrawat@gmail.com>
2017-09-17 15:50:21 +0530

    add a method for our type

--- GoGuide/Cards/deck.go
+++ GoGuide/Cards/deck.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 // Create a new type of 'deck'
 // which is a slice of strings
 type deck []string
+
+// print loops through a deck of cards
+// and print out the value of every single card
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
