0c701e2e837b7e5aaa14c0c74ff8eb6df75b70a4
Mukul Rawat <mukulsrawat@gmail.com>
2017-09-20 15:22:11 +0530

    add a newDeck function to create a deck

--- GoGuide/Cards/deck.go
+++ GoGuide/Cards/deck.go
@@ -13,3 +13,20 @@ func (d deck) print() {
 		fmt.Println(i, card)
 	}
 }
+
+// Create and return a list of playing cards.
+// Essentially an array of strings.
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
