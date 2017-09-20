63084e8ab3a05e8bd93fbe8e5e74f5223b408745
Mukul Rawat <mukulsrawat@gmail.com>
2017-09-20 22:09:02 +0530

    finish testing newDeck function

--- GoGuide/Cards/deck_test.go
+++ GoGuide/Cards/deck_test.go
@@ -9,4 +9,14 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16. but got %d", len(d))
 	}
+
+	// make sure first card is an ace of spades
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades but got %v", d[0])
+	}
+
+	// make sure that the last card is four of clubs
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
 }
