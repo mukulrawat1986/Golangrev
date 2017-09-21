e5721d2932d289958fe5ff7d9f900415175cb3da
Mukul Rawat <mukulsrawat@gmail.com>
2017-09-21 09:09:15 +0530

    declaring a map using literal syntax

---
+++ GoGuide/Map/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// map using literal syntax
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+	}
+	fmt.Println(colors)
+}
