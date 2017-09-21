4f28219b76abab8bd704dd93942ab87a1722b651
Mukul Rawat <mukulsrawat@gmail.com>
2017-09-21 09:20:39 +0530

    declare map using make

--- GoGuide/Map/main.go
+++ GoGuide/Map/main.go
@@ -5,13 +5,18 @@ import "fmt"
 func main() {
 
 	// declare a map using var syntax
-	var colors map[string]string
+	// var colors map[string]string
+
+	// declare a map using make
+	colors := make(map[string]string)
 
 	// map using literal syntax
 	// colors := map[string]string{
 	// 	"red":   "#ff0000",
 	// 	"green": "#4bf745",
 	// }
+
+	colors["white"] = "#ffffff"
 
 	fmt.Println(colors)
 }
