ef7ade294175405fbde91e056b727ef125bae91d
Mukul Rawat <mukulsrawat@gmail.com>
2017-09-21 09:19:26 +0530

    declare map using var syntax

--- GoGuide/Map/main.go
+++ GoGuide/Map/main.go
@@ -4,10 +4,14 @@ import "fmt"
 
 func main() {
 
+	// declare a map using var syntax
+	var colors map[string]string
+
 	// map using literal syntax
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
 	fmt.Println(colors)
 }
