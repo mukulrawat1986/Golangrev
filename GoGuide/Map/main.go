c6447298fa9b223db5d1ca9139ed5f6635ed804a
Mukul Rawat <mukulsrawat@gmail.com>
2017-09-21 09:40:47 +0530

    iterating over a map

--- GoGuide/Map/main.go
+++ GoGuide/Map/main.go
@@ -4,22 +4,17 @@ import "fmt"
 
 func main() {
 
-	// declare a map using var syntax
-	// var colors map[string]string
-
-	// declare a map using make
-	colors := make(map[string]string)
-
 	// map using literal syntax
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+	printMap(colors)
+}
 
-	colors["white"] = "#ffffff"
-
-	// delete key
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
