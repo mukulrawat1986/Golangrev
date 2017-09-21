b8b0f37295b1a2a664429cb755131ca047ee31c6
Mukul Rawat <mukulsrawat@gmail.com>
2017-09-21 15:50:18 +0530

    write a custom writer

--- GoGuide/HTTP/main.go
+++ GoGuide/HTTP/main.go
@@ -7,12 +7,21 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
+}
+
 func main() {
-	resp, err := http.Get("http://www.gooogle.com")
+	resp, err := http.Get("http://www.google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
 }
