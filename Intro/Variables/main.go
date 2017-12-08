48858e04835a299681b4c5781461845aef996bbe
Mukul Rawat <mukulsrawat@gmail.com>
2017-12-08 10:10:10 +0530

    example of constants

--- Intro/Variables/main.go
+++ Intro/Variables/main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
-func main() {
-	message := "The answer to life is %d\n"
-	answer := 42
-	fmt.Printf(message, answer)
+// declare global constants
+// don't need := here as we are declaring it by having
+// const here
+const (
+	message = "The answer to life is %d\n"
+	answer  = 42
+)
 
+func main() {
+	fmt.Printf(message, answer)
 }
