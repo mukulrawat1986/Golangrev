9f821ab250b905c8dc50a368fb4a5dfc10dd23ca
Mukul Rawat <mukulsrawat@gmail.com>
2017-12-10 10:10:10 +0530

    using back quotes

--- Intro/String/main.go
+++ Intro/String/main.go
@@ -5,18 +5,7 @@ import (
 )
 
 func main() {
-	atoz := "the quick brown fox jumps over the lazy dog"
-	fmt.Printf("%s\n", atoz[15:])
-
-	// traversing a string rune by rune
-	// range when applied to a string does utf-8 decoding
-	// implicitly
-	for i, r := range atoz {
-		fmt.Printf("%d %c\n", i, r)
-	}
-
-	// get the length of the string
-	// note: len returns the number of bytes in a string
-	fmt.Printf("%d\n", len(atoz))
+	atoz := `the quick brown fox jumps over the lazy dog\n`
+	fmt.Printf("%s\n", atoz)
 
 }
