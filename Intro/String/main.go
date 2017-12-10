93628c0df5003d841ec899ac0034756c3c953321
Mukul Rawat <mukulsrawat@gmail.com>
2017-12-10 10:10:10 +0530

    iterating over a string rune by rune

--- Intro/String/main.go
+++ Intro/String/main.go
@@ -7,4 +7,12 @@ import (
 func main() {
 	atoz := "the quick brown fox jumps over the lazy dog"
 	fmt.Printf("%s\n", atoz[15:])
+
+	// traversing a string rune by rune
+	// range when applied to a string does utf-8 decoding
+	// implicitly
+	for i, r := range atoz {
+		fmt.Printf("%d %c\n", i, r)
+	}
+
 }
