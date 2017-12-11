133a3cb486f58ff1f1b24fe2e31f125b8267f81e
Mukul Rawat <mukulsrawat@gmail.com>
2017-12-11 10:10:10 +0530

    switching on a variable

--- Intro/Switch/main.go
+++ Intro/Switch/main.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
-	n, err := fmt.Printf("Hello, World!\n")
+	atoz := "the quick brown fox jumps over the lazy dog"
 
-	n = 0
+	vowels := 0
+	consonants := 0
 
-	switch {
-	case err != nil:
-		os.Exit(1)
-	case n == 0:
-		fmt.Printf("No bytes output\n")
-		fallthrough
-	case n != 14:
-		fmt.Printf("Wrong number of characters\n")
-	default:
-		fmt.Printf("OK!\n")
+	for _, r := range atoz {
+		switch r {
+		case 'a', 'e', 'i', 'o', 'u':
+			vowels += 1
+		default:
+			consonants += 1
+		}
 	}
+
+	fmt.Printf("Vowels: %d; Consonants: %d\n", vowels, consonants)
 }
