00f0179febaa91c5a503ed57546265a4ec57689a
Mukul Rawat <mukulsrawat@gmail.com>
2017-12-15 10:10:10 +0530

    Map examples

---
+++ Intro/Maps/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	dayMonths := make(map[string]int)
+	dayMonths["Jan"] = 31
+	dayMonths["Feb"] = 28
+	dayMonths["Mar"] = 31
+	dayMonths["Apr"] = 30
+	dayMonths["May"] = 31
+	dayMonths["Jun"] = 30
+	dayMonths["Jul"] = 31
+	dayMonths["Aug"] = 31
+	dayMonths["Sep"] = 30
+	dayMonths["Oct"] = 31
+	dayMonths["Nov"] = 30
+	dayMonths["Dec"] = 31
+
+	fmt.Printf("Days in February :%d\n",
+		dayMonths["Feb"])
+
+	days, ok := dayMonths["January"]
+	if !ok {
+		fmt.Printf("Can't get days for January\n")
+	} else {
+		fmt.Printf("%d\n", days)
+	}
+
+	for month, days := range dayMonths {
+		fmt.Printf("%s has %d days\n",
+			month, days)
+	}
+}
