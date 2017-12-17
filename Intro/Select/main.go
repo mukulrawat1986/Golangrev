8eab0217594dcd48f33e115cdc345296aa1d7523
Mukul Rawat <mukulsrawat@gmail.com>
2017-12-17 10:10:10 +0530

    example of using select and stopping loop using a boolean channel

---
+++ Intro/Select/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func emit(wordChannel chan string, done chan bool) {
+	words := []string{"The", "quick", "brown",
+		"fox"}
+
+	var i int
+
+	for {
+		select {
+		case wordChannel <- words[i]:
+			i++
+			if i == len(words) {
+				i = 0
+			}
+		case <-done:
+			fmt.Printf("\nGot done\n")
+			close(done)
+			return
+		}
+	}
+}
+
+func main() {
+	wordCh := make(chan string)
+	doneCh := make(chan bool)
+
+	go emit(wordCh, doneCh)
+
+	for i := 0; i < 100; i++ {
+		fmt.Printf("%s ", <-wordCh)
+	}
+
+	doneCh <- true
+}
