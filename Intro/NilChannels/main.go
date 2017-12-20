2176981b83268953fde9cb4c7a3722ad85e79837
Mukul Rawat <mukulsrawat@gmail.com>
2017-12-20 10:10:10 +0530

    example of a nil channel

---
+++ Intro/NilChannels/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Nil channles don't block when we try to receive from it
+
+func reader(ch chan int) {
+	t := time.NewTimer(3 * time.Second)
+	for {
+		select {
+		case i := <-ch:
+			fmt.Printf("%d\n", i)
+
+		case <-t.C:
+			ch = nil
+		}
+	}
+}
+
+func writer(ch chan int) {
+	for {
+		ch <- rand.Intn(42)
+	}
+}
+
+func main() {
+	ch := make(chan int)
+
+	go reader(ch)
+	go writer(ch)
+
+	time.Sleep(10 * time.Second)
+}
