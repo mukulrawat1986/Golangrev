fc589e5816c08013c9bbbbe4b32dde7db3cc76fe
Mukul Rawat <mukulsrawat@gmail.com>
2017-12-21 10:10:10 +0530

    create a type and write a method for the type

---
+++ Intro/Types/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type webPage struct {
+	url  string
+	body []byte
+	err  error
+}
+
+func (w *webPage) get() {
+	resp, err := http.Get(w.url)
+	if err != nil {
+		w.err = err
+		return
+	}
+	defer resp.Body.Close()
+
+	w.body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		w.err = err
+		return
+	}
+}
+
+func main() {
+	w := &webPage{url: "http://www.oreilly.com/"}
+	w.get()
+	fmt.Printf("URL: %s Error:%s Length:%d\n", w.url, w.err, len(w.body))
+}
