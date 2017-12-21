ddb72e92b1f1c249dd7d3905b42b2d59a432f3b0
Mukul Rawat <mukulsrawat@gmail.com>
2017-12-21 10:10:10 +0530

    convert a slice of int to a new type

--- Intro/Types/main.go
+++ Intro/Types/main.go
@@ -31,6 +31,16 @@ func (w *webPage) isOk() bool {
 	return w.err == nil
 }
 
+type SummableSlice []int
+
+func (s SummableSlice) sum() int {
+	sum := 0
+	for _, i := range s {
+		sum += i
+	}
+	return sum
+}
+
 func main() {
 	w := &webPage{url: "http://www.oreilly.com/"}
 	w.get()
@@ -39,4 +49,7 @@ func main() {
 	} else {
 		fmt.Printf("Something went wrong\n")
 	}
+
+	var s SummableSlice = SummableSlice{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Printf("Sum is %d\n", s.sum())
 }
