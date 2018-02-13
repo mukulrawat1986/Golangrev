5ce9fcabe01070b16136a69a146fabdae9e01215
Mukul Rawat <mukulsrawat@gmail.com>
2018-02-13 10:10:10 +0530

    using := to declare variables

--- Intro/Variables/main.go
+++ Intro/Variables/main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var message string
-
-	message = "Hello, World\n"
+	// := is declaration while = is assignment
+	message := "Hello, World\n"
 
 	fmt.Printf(message)
 }
