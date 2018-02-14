8e7cee7447f1fff4da70ec51af71340f208cdf2d
Mukul Rawat <mukulsrawat@gmail.com>
2018-02-14 10:10:10 +0530

    going through a string rune by rune and finding number of bytes in a string

--- Intro/Strings/main.go
+++ Intro/Strings/main.go
@@ -5,7 +5,18 @@ import "fmt"
 func main() {
 	atoz := "the quick brown fox jumps over the lazy dog\n"
 
-	fmt.Printf("%s\n", atoz[0:9])
-	fmt.Printf("%s\n", atoz[15:19])
-	fmt.Printf("%s\n", atoz[:19])
+	// going through a string rune by rune
+	for _, r := range atoz {
+		fmt.Printf("%c\n", r)
+	}
+
+	// going through a string rune by rune
+	s := "Hello 世界"
+	for i, r := range s {
+		fmt.Printf("%d %c\n", i, r)
+	}
+
+	// number of bytes in a string
+	fmt.Printf("Total bytes in %s = %d\n", s, len(s))
+
 }
