f4e3b8ee89eca3b11f0f9bcc9e579821be31d925
Mukul Rawat <mukulsrawat@gmail.com>
2018-02-14 10:10:10 +0530

    using backquotes

--- Intro/Strings/main.go
+++ Intro/Strings/main.go
@@ -3,20 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	atoz := "the quick brown fox jumps over the lazy dog\n"
+	atoz := `the quick brown fox jumps over the lazy dog\n`
 
-	// going through a string rune by rune
-	for _, r := range atoz {
-		fmt.Printf("%c\n", r)
-	}
-
-	// going through a string rune by rune
-	s := "Hello 世界"
-	for i, r := range s {
-		fmt.Printf("%d %c\n", i, r)
-	}
-
-	// number of bytes in a string
-	fmt.Printf("Total bytes in %s = %d\n", s, len(s))
-
+	fmt.Printf("%s\n", atoz)
 }
