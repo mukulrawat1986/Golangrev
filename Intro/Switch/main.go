c2c333da82a0e5f024f8aaca40353b31aa628a00
Mukul Rawat <mukulsrawat@gmail.com>
2018-02-15 10:10:10 +0530

    Example of fallthrough

--- Intro/Switch/main.go
+++ Intro/Switch/main.go
@@ -8,11 +8,14 @@ import (
 func main() {
 	n, err := fmt.Printf("Hello, World!\n")
 
+	n = 0
+
 	switch {
 	case err != nil:
 		os.Exit(1)
 	case n == 0:
 		fmt.Printf("No bytes output\n")
+		fallthrough
 	case n != 14:
 		fmt.Printf("Wrong number of characters")
 	default:
