24e9c069291839f65bc5055a4aa6d493f5e658ba
Mukul Rawat <mukulsrawat@gmail.com>
2018-02-15 10:10:10 +0530

    Switching over a variable with fallthrough

--- Intro/Switch/main.go
+++ Intro/Switch/main.go
@@ -5,16 +5,19 @@ import "fmt"
 func main() {
 	atoz := "the quick brown fox jumps over the lazy dog"
 
-	vowels, consonants := 0, 0
+	vowels, consonants, zeds := 0, 0, 0
 
 	for _, r := range atoz {
 		switch r {
 		case 'a', 'e', 'i', 'o', 'u':
 			vowels += 1
+		case 'z':
+			zeds += 1
+			fallthrough
 		default:
 			consonants += 1
 		}
 	}
 
-	fmt.Printf("Vowels: %d, Consonants: %d\n", vowels, consonants)
+	fmt.Printf("Vowels: %d, Consonants: %d (Zeds: %d)\n", vowels, consonants, zeds)
 }
