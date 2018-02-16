07bd97c83abe491a44878bca8228953e956c4f9c
Mukul Rawat <mukulsrawat@gmail.com>
2018-02-16 10:10:10 +0530

    using for loop

--- Intro/For/main.go
+++ Intro/For/main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var counter int
-	counter = 0
-	for counter < 10 {
+	for counter := 0; counter < 10; counter++ {
 		fmt.Printf("Hello, World!\n")
-		counter++
 	}
 }
