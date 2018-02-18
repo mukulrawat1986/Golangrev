9099fb8a65720b5815b4e62ef257c20476ca1f78
Mukul Rawat <mukulsrawat@gmail.com>
2018-02-18 10:10:10 +0530

    fixed size array

--- Intro/Arrays/main.go
+++ Intro/Arrays/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// array with unknown number of values of string
-	words := [...]string{"the", "quick", "brown", "fox"}
+
+	words := [4]string{"the", "quick", "brown", "fox"}
 
 	fmt.Printf("%s\n", words[2])
 }
