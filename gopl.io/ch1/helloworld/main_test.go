c103a4cc2820747e4628cbd0304a1d04b22c7b9e
Mukul Rawat <mukulsrawat@gmail.com>
2018-02-28 10:10:10 +0530

    test the hello function

---
+++ gopl.io/ch1/helloworld/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	// create buffer of bytes to simulate io.Writer
+	buffer := bytes.Buffer{}
+
+	// the msg to be passed to the function
+	msg := "Hello World"
+	Hello(buffer, msg)
+
+	got := buffer.String()
+	want := msg
+
+	if got != want {
+		t.Errorf("got '%s' want '%s'")
+	}
+}
