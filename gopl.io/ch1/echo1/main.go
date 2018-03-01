3a0557ddefa9687ffdaf33f534bd2a66f7530a1f
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-01 10:10:10 +0530

    implement the Echo function

---
+++ gopl.io/ch1/echo1/main.go
@@ -0,0 +1,21 @@
+// Echo1 prints its command line arguments
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Echo(w io.Writer, args []string) {
+	var s, sep string
+	for i := 1; i < len(args); i++ {
+		s += sep + args[i]
+		sep = " "
+	}
+	fmt.Fprintln(w, s)
+}
+
+func main() {
+	Echo(os.Stdout, os.Args)
+}
