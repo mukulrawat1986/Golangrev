b837d5e527416bc3d76652d9385968dd20ab6a88
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-02 10:10:10 +0530

    implement the Echo function

--- gopl.io/ch1/echo3/main.go
+++ gopl.io/ch1/echo3/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func Echo(w io.Writer, args []string) {
-
+	res := strings.Join(args[1:], " ")
+	fmt.Fprintln(w, res)
 }
 
 func main() {
