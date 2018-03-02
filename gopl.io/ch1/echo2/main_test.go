994a13bca16b678dbb36e347ecaa56eaa1b72fb9
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-02 10:10:10 +0530

    write test for echo when command line arguments are given

---
+++ gopl.io/ch1/echo2/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestEcho(t *testing.T) {
+	// we will mock command line arguments by using a slice of strings.
+	// The output will be stored in a bytes buffer
+
+	t.Run("when command line arguments are given", func(t *testing.T) {
+		args := []string{"filename", "hello", "world"}
+
+		// create a bytes buffer to store output
+		buffer := bytes.Buffer{}
+
+		// pass it to the function
+		Echo(&buffer, args)
+
+		got := buffer.String()
+		want := args[1] + " " + args[2] + "\n"
+
+		if got != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	})
+}
