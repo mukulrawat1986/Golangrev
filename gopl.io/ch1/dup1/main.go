57cb37832dd99d04b2762a00691ef24d08279b35
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-03 10:10:10 +0530

    write the structure of the program and implement the Count function

---
+++ gopl.io/ch1/dup1/main.go
@@ -0,0 +1,27 @@
+// Dup1 prints the text of each line that appears more than once
+// in the standard input, preceeded by its count.
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+)
+
+func main() {
+	// create a map to hold the count
+	counts := make(map[string]int)
+
+	// call the function to count
+	Count(counts, os.Stdin)
+
+	// call the function to print the count
+	Print(os.Stdout, counts)
+}
+
+func Count(counts map[string]int, r io.Reader) {
+	input := bufio.NewScanner(r)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
