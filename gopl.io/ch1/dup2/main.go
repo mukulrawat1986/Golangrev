ef7e8e8fc3ec944fb6fc670f8069dc4cefd39dd9
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-06 10:10:10 +0530

    finish implementation details

--- gopl.io/ch1/dup2/main.go
+++ gopl.io/ch1/dup2/main.go
@@ -2,9 +2,28 @@
 // in the input. It reads from stdin or from a list of named files.
 package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		Count(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			defer f.Close()
+			Count(f, counts)
+		}
+	}
 }
 
 func Count(r io.Reader, counts map[string]int) {
