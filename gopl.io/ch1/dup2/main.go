1eb5c9e788ffb735811a59d13a2ca772ea38e79b
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-06 10:10:10 +0530

    write the basic design for the program

---
+++ gopl.io/ch1/dup2/main.go
@@ -0,0 +1,14 @@
+// dup2 prints the count and test of lines that appear more than once
+// in the input. It reads from stdin or from a list of named files.
+package main
+
+import "io"
+
+func main() {
+}
+
+func Count(r io.Reader, counts map[string]int) {
+}
+
+func Print(w io.Writer, counts map[string]int) {
+}
