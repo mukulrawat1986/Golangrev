99e86027e6d19dccf587154f2f83347f972b1a6f
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-09 10:10:10 +0530

    finish our Echo function

--- gopl.io/ch1/Echo1/main.go
+++ gopl.io/ch1/Echo1/main.go
@@ -2,12 +2,18 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 )
 
 func Echo(w io.Writer, args []string) {
-
+	var s, sep string
+	for i := 1; i < len(args); i++ {
+		s += sep + args[i]
+		sep = " "
+	}
+	fmt.Fprintf(w, "%s\n", s)
 }
 
 func main() {
