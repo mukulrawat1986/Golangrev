e81b736012b5fd0a5e7d25130ce85bbb2b1234c0
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-10 10:10:10 +0530

    write the code for Echo function

--- gopl.io/ch1/Echo2/main.go
+++ gopl.io/ch1/Echo2/main.go
@@ -2,12 +2,18 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 )
 
 func Echo(w io.Writer, args []string) {
-
+	s, sep := "", ""
+	for _, arg := range args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Fprintf(w, "%s\n", s)
 }
 
 func main() {
