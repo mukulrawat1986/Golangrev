b0c0d7e437c6cb869166d46691989af62287d35f
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-13 10:10:10 +0530

    implement our Greet function

--- gopl.io/ch1/helloworld/main.go
+++ gopl.io/ch1/helloworld/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 )
 
 // Greet function writes a greeting to the writer
 func Greet(writer *bytes.Buffer, str string) {
-
+	fmt.Fprintf(writer, "%s\n", str)
 }
 
 func main() {
