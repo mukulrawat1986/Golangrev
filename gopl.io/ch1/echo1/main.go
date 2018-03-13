0796fffb6d1132b493f289bf415b36b0c275a9fb
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-13 10:10:10 +0530

    declare our Echo function

---
+++ gopl.io/ch1/echo1/main.go
@@ -0,0 +1,13 @@
+// Echo prints its command line arguments.
+package main
+
+import "io"
+
+// Echo prints the command line arguments passed to it
+func Echo(writer io.Writer, args []string) {
+
+}
+
+func main() {
+
+}
