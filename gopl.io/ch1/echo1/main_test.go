a8e881f0bc109056950952b05eb1a5f158efc1aa
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-13 10:10:10 +0530

    use subtests

--- gopl.io/ch1/echo1/main_test.go
+++ gopl.io/ch1/echo1/main_test.go
@@ -6,18 +6,20 @@ import (
 )
 
 func TestEcho(t *testing.T) {
-	// create a buffer of bytes.Buffer which we will inject as our
-	// dependency for printing.
-	buffer := bytes.Buffer{}
+	t.Run("when arguments are passed", func(t *testing.T) {
+		// create a buffer of bytes.Buffer which we will inject as our
+		// dependency for printing.
+		buffer := bytes.Buffer{}
 
-	// a slice to mock our command line arguments
-	args := []string{"filename", "Hello", "World"}
+		// a slice to mock our command line arguments
+		args := []string{"filename", "Hello", "World"}
 
-	Echo(&buffer, args)
+		Echo(&buffer, args)
 
-	got := buffer.String()
-	want := args[1] + " " + args[2] + "\n"
-	if got != want {
-		t.Errorf("got %#v want %#v", got, want)
-	}
+		got := buffer.String()
+		want := args[1] + " " + args[2] + "\n"
+		if got != want {
+			t.Errorf("got %#v want %#v", got, want)
+		}
+	})
 }
