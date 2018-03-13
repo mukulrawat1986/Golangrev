a8b896c97a7bfb90979e263fd54f14cdd5dd83fc
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-13 10:10:10 +0530

    write test for the Echo function

---
+++ gopl.io/ch1/echo1/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestEcho(t *testing.T) {
+	// create a buffer of bytes.Buffer which we will inject as our
+	// dependency for printing.
+	buffer := bytes.Buffer{}
+
+	// a slice to mock our command line arguments
+	args := []string{"filename", "Hello", "World"}
+
+	Echo(&buffer, args)
+
+	got := buffer.String()
+	want := args[1] + " " + args[2] + "\n"
+	if got != want {
+		t.Errorf("got %#v want %#v", got, want)
+	}
+}
