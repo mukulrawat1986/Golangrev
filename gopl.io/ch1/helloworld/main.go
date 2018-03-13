e3bd603b9cba49126ead5247adb506e3a230c736
Mukul Rawat <mukulsrawat@gmail.com>
2018-03-13 10:10:10 +0530

    refactor to use a more general purpose interface and a variable for our greeting

--- gopl.io/ch1/helloworld/main.go
+++ gopl.io/ch1/helloworld/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
+)
+
+var (
+	greeting = "Hello, World!"
 )
 
 // Greet function writes a greeting to the writer
-func Greet(writer *bytes.Buffer, str string) {
+func Greet(writer io.Writer, str string) {
 	fmt.Fprintf(writer, "%s\n", str)
 }
 
 func main() {
-
+	Greet(os.Stdout, greeting)
 }
