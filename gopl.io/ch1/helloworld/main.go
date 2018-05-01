7a4f3f84c2d32743e9c2ac7b08937e6119881741
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-01 10:10:10 +0530

    Implement the Hello function

--- gopl.io/ch1/helloworld/main.go
+++ gopl.io/ch1/helloworld/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
 
 // Hello function greets the person or prints out "Hello World"
 func Hello(b *bytes.Buffer, name string) {
-
+	fmt.Fprintf(b, "Hello %s!\n", name)
 }
 
 func main() {
