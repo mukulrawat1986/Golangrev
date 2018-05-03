dc4e6790760533f58bd2187df1e6a0b746600345
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-03 10:10:10 +0530

    write test for when no arguments are given

--- gopl.io/ch1/Echo3/main_test.go
+++ gopl.io/ch1/Echo3/main_test.go
@@ -27,4 +27,21 @@ func TestEcho(t *testing.T) {
 			t.Errorf("got '%#v' want '%#v'", got, want)
 		}
 	})
+
+	t.Run("when no arguments are given", func(t *testing.T) {
+		// create a buffer
+		buffer := bytes.Buffer{}
+
+		// create a slice of arguments
+		args := []string{"filename"}
+
+		Echo(&buffer, args)
+
+		got := buffer.String()
+		want := "\n"
+
+		if got != want {
+			t.Errorf("got '%#v' want '%#v'", got, want)
+		}
+	})
 }
