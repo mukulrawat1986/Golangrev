bbffdebf6f506e9e262df4df59e107c85aaffb9a
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-04 10:10:10 +0530

    Implement Dup function to print the count and text of lines from the input

--- gopl.io/ch1/dup2/main.go
+++ gopl.io/ch1/dup2/main.go
@@ -1,11 +1,35 @@
 package main
 
-import "io"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 // Dup prints the count and text of lines that appear more than once
 // in the input. It reads from the stdin or from a list of named files.
 func Dup(in io.Reader, out io.Writer) {
 
+	// counts store the string and the count of it
+	counts := make(map[string]int)
+
+	// create a scanner object on the in io.Reader
+	input := bufio.NewScanner(in)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	if err := input.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "dup:%v\n", err)
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Fprintf(out, "%s\t%d\n", line, n)
+		}
+	}
 }
 
 func main() {
