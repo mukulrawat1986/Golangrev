2e60c3fa4c44e9446ff5987a34ca96d0b70e3745
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-04 10:10:10 +0530

    update the comment

--- gopl.io/ch1/dup2/main.go
+++ gopl.io/ch1/dup2/main.go
@@ -2,7 +2,7 @@ package main
 
 import "io"
 
-// Dups prints the count and text of lines that appear more than once
+// Dup prints the count and text of lines that appear more than once
 // in the input. It reads from the stdin or from a list of named files.
 func Dup(in io.Reader, out io.Writer) {
 
