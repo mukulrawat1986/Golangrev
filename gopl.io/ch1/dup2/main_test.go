d347871a7d928d0b755181ebbcb74b8d15daa939
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-04 10:10:10 +0530

    refactor the test and use comparison of maps instead of strings

--- gopl.io/ch1/dup2/main_test.go
+++ gopl.io/ch1/dup2/main_test.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"bytes"
+	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 )
 
 func TestDup(t *testing.T) {
+
+	// private function to convert our string to a map.
+	makeMap := func(input string) map[string]int {
+		result := make(map[string]int)
+		in := strings.Split(input, "\n")
+
+		for _, str := range in {
+			if str != "" {
+				temp := strings.Split(str, "\t")
+				val, _ := strconv.Atoi(temp[1])
+				result[temp[0]] = val
+			}
+		}
+		return result
+	}
+
 	// create a buffer to store output of type bytes.Buffer
 	outBuffer := &bytes.Buffer{}
 
@@ -17,10 +36,13 @@ func TestDup(t *testing.T) {
 
 	Dup(inBuffer, outBuffer)
 
-	got := outBuffer.String()
-	want := "Hello\t3\nhello\t2\n"
+	got := makeMap(outBuffer.String())
+	want := map[string]int{
+		"Hello": 3,
+		"hello": 2,
+	}
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got '%#v' want '%#v'", got, want)
 	}
 }
