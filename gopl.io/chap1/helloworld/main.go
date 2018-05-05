4a8d391178ba83b87e2ef601447944f4c34526d4
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-05 10:10:10 +0530

    refactor the Hello function and pass the test

--- gopl.io/chap1/helloworld/main.go
+++ gopl.io/chap1/helloworld/main.go
@@ -8,7 +8,11 @@ import (
 
 // Hello function greets the name passed.
 func Hello(buffer io.Writer, name string) {
-	fmt.Fprintf(buffer, "Hello %s\n", name)
+	if name == "" {
+		fmt.Fprintln(buffer, "Hello World!")
+	} else {
+		fmt.Fprintf(buffer, "Hello %s\n", name)
+	}
 }
 
 func main() {
