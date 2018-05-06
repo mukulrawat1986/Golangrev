85b55e2113ff7276d108e3fb33f8950b8f110fc3
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-06 10:10:10 +0530

    convert our test to use subtests

--- gopl.io/chap1/helloworld/main_test.go
+++ gopl.io/chap1/helloworld/main_test.go
@@ -6,17 +6,20 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	// create a buffer of bytes.Buffer to store the output.
-	// this buffer is passed as an argument in the test call.
-	buffer := bytes.Buffer{}
 
-	// Hello function called
-	Hello(&buffer, "Chris")
+	t.Run("when a name is given", func(t *testing.T) {
+		// create a buffer of bytes.Buffer to store the output.
+		// this buffer is passed as an argument in the test call.
+		buffer := bytes.Buffer{}
 
-	got := buffer.String()
-	want := "Hello, Chris!\n"
+		// Hello function called
+		Hello(&buffer, "Chris")
 
-	if got != want {
-		t.Errorf("got %#v want %#v", got, want)
-	}
+		got := buffer.String()
+		want := "Hello, Chris!\n"
+
+		if got != want {
+			t.Errorf("got %#v want %#v", got, want)
+		}
+	})
 }
