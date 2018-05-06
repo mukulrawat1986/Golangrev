94ed7a84bcda1304668bf7274ff1dacec8f69730
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-06 10:10:10 +0530

    write test for Hello function

---
+++ gopl.io/chap1/helloworld/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	// create a buffer of bytes.Buffer to store the output.
+	// this buffer is passed as an argument in the test call.
+	buffer := bytes.Buffer{}
+
+	// Hello function called
+	Hello(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris!"
+
+	if got != want {
+		t.Errorf("got %#v want %#v", got, want)
+	}
+}
