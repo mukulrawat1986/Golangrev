1954df5a79baab9b301910d19e9b3260930774f3
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-06 10:10:10 +0530

    write minimal code to pass the tests

--- gopl.io/chap1/helloworld/main.go
+++ gopl.io/chap1/helloworld/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 )
 
 // Hello function greets a named string
 func Hello(writer *bytes.Buffer, name string) {
-
+	fmt.Fprintf(writer, "Hello, %s!\n", name)
 }
 
 func main() {
