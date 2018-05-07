edf7be939f4c92dc8464b395e48c13edede241a6
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-07 10:10:10 +0530

    add test for when no arguments are given

--- gopl.io/chap1/echo3/main_test.go
+++ gopl.io/chap1/echo3/main_test.go
@@ -28,4 +28,24 @@ func TestEcho(t *testing.T) {
 			t.Errorf("got %#v want %#v", got, want)
 		}
 	})
+
+	t.Run("when an empty string is passed as argument", func(t *testing.T) {
+		// create a buffer to store the print output
+		buffer := bytes.Buffer{}
+
+		// use a slice of string to store the command line arguments
+		args := []string{
+			"filename",
+		}
+
+		// Echo function called
+		Echo(&buffer, args)
+
+		got := buffer.String()
+		want := "\n"
+
+		if got != want {
+			t.Errorf("got %#v want %#v", got, want)
+		}
+	})
 }
