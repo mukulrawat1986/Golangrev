f58a4d103d2d8f5068951d289386dd28597e6484
Mukul Rawat <mukulsrawat@gmail.com>
2018-05-08 10:10:10 +0530

    declare the Dup function

---
+++ gopl.io/chap1/dup1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "io"
+
+// Dup prints the text of each line that appears more than once
+// in the standard input, preceeded by its count.
+func Dup(i io.Writer, w io.Writer) {
+
+}
+
+func main() {
+
+}
