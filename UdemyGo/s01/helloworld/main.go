b485d6a1816358a921cbc728cd6fdba1b8ba071e
mukulrawat1986 <mukulsrawat@gmail.com>
2018-05-09 11:11:11 +0530

    variable are case sensitive

--- UdemyGo/s01/helloworld/main.go
+++ UdemyGo/s01/helloworld/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	concepts()
+}
+
+func concepts() {
+	k := 12
+	K := 14
+
+	// difference between upper and lower case
+	fmt.Println(k, K)
 }
