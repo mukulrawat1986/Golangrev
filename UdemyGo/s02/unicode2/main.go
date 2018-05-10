1c55a581052d5a32b8aab2186dee909040e7eb81
mukulrawat1986 <mukulsrawat@gmail.com>
2018-05-10 11:11:11 +0530

    revising more of unicode characters and escape characters

---
+++ UdemyGo/s02/unicode2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("こんにちは世界")
+	fmt.Println("\"OK\"")
+	fmt.Println(`\"OK\"`)
+	fmt.Println(`"OK"`)
+
+	fmt.Println(`
+	<!DOCTYPE html>
+	<html>
+	<body>
+		<h1> My First Heading </h1>
+	<body>
+	</html>`)
+}
