57bbee7355a613b66d32668bcd23e27d931ed92d
mukulrawat1986 <mukulsrawat@gmail.com>
2018-05-10 11:11:11 +0530

    using only one argument with multiple format specifiers

--- UdemyGo/s02/binary_decimal_octal_hex/main.go
+++ UdemyGo/s02/binary_decimal_octal_hex/main.go
@@ -6,5 +6,10 @@ func main() {
 
 	a := byte('A')
 
+	// showing print format specifier for decimal, octal, hex
+	// and binary.
 	fmt.Printf("%d %o %x %b \n", a, a, a, a)
+
+	// using only one argument with multiple format specifiers
+	fmt.Printf("%d %[1]o %[1]x %[1]b", a)
 }
