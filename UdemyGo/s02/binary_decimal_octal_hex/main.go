b128da402dde5edacf48adf20c674849aaf236e4
mukulrawat1986 <mukulsrawat@gmail.com>
2018-05-10 11:11:11 +0530

    Implicit conversion from a rune to a byte

--- UdemyGo/s02/binary_decimal_octal_hex/main.go
+++ UdemyGo/s02/binary_decimal_octal_hex/main.go
@@ -11,5 +11,8 @@ func main() {
 	fmt.Printf("%d %o %x %b \n", a, a, a, a)
 
 	// using only one argument with multiple format specifiers
-	fmt.Printf("%d %[1]o %[1]x %[1]b", a)
+	fmt.Printf("%d %[1]o %[1]x %[1]b\n", a)
+
+	// let go implicity convert from a rune to a byte
+	fmt.Printf("%d %o %x %b\n", 'A', 'A', 'A', 'A')
 }
