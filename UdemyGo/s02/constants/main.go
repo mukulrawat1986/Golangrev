9c27b9e1cb68db16668ed8fdbaad1379ce7696e4
mukulrawat1986 <mukulsrawat@gmail.com>
2018-05-10 11:11:11 +0530

    declaring and assigning multiple constants at once

--- UdemyGo/s02/constants/main.go
+++ UdemyGo/s02/constants/main.go
@@ -16,4 +16,18 @@ func main() {
 	// k1 = "changing type is not allowed" // compile error
 
 	fmt.Println(25, pi, k1)
+
+	// declaring and assigning multiple constants at once
+	// the constants get the same value as their preceeding constant
+	// unless its changed.
+	const (
+		a = 2
+		b
+		c
+		d = a * 10
+		e
+		f
+	)
+
+	fmt.Println(a, b, c, d, e, f)
 }
