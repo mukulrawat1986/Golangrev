5b23e48bd57b634a4d92750863b0f53d241a2f4d
mukulrawat1986 <mukulsrawat@gmail.com>
2018-05-11 11:11:11 +0530

    more on runes

---
+++ UdemyGo/s02/utf8_5/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	s := "こんにちは世界"
+
+	fmt.Printf("% x\n\n", s)
+
+	r := []rune(s)
+	fmt.Println(r)
+	fmt.Printf("%x\n", r)
+
+	fmt.Println(string(r))
+}
