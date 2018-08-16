1a86adafa26c4dc8f1435fb3e0bb4519da5323c9
mukulrawat1986 <mukulsrawat@gmail.com>
2018-08-16 11:11:11 +0530

    Add two new handlers in for new paths. We will just put placeholder statments in those handlers.

--- snippetbox.org/cmd/app/handlers.go
+++ snippetbox.org/cmd/app/handlers.go
@@ -9,3 +9,13 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write([]byte("Hello from Snippetbox\n"))
 }
+
+// Add a placeholder ShowSnippet Handler function
+func ShowSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet...\n"))
+}
+
+// Add a placeholder NewSnipper Handler function
+func NewSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display the new snippet form....\n"))
+}
--- snippetbox.org/cmd/app/main.go
+++ snippetbox.org/cmd/app/main.go
@@ -9,6 +9,8 @@ func main() {
 	// create a new servemux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Home)
+	mux.HandleFunc("/snippet", ShowSnippet)
+	mux.HandleFunc("/snippet/new", NewSnippet)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
