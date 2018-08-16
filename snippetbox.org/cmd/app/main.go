908e3de21f22797e66e0e697f58a21fc0ee6380a
mukulrawat1986 <mukulsrawat@gmail.com>
2018-08-16 11:11:11 +0530

    Serve the static files
    We use http.FileServer function which serves files from a specific
    directory and returns us a handler. We use that handler to map tp
    "/static/".

--- snippetbox.org/cmd/app/main.go
+++ snippetbox.org/cmd/app/main.go
@@ -12,6 +12,17 @@ func main() {
 	mux.HandleFunc("/snippet", ShowSnippet)
 	mux.HandleFunc("/snippet/new", NewSnippet)
 
+	// create a file server which serves files out of "./web/static"
+	// The path is relative to our project repository root
+	fileServer := http.FileServer(http.Dir("./web/static"))
+
+	// use mux.Handle to register fileServer as a handler for all url
+	// paths that start with "/static/".
+	// For matching paths, we strip the /static/ prefix before the
+	// request reaches the filleServer handler
+	mux.Handle("/static/", http.StripPrefix("/static",
+		fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
