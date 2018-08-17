d2517d97163915d3f1316af91a5441c8fdb9e017
mukulrawat1986 <mukulsrawat@gmail.com>
2018-08-17 11:11:11 +0530

    correct the spelling and add StaticDir as a dependency

--- snippetbox.org/cmd/app/app.go
+++ snippetbox.org/cmd/app/app.go
@@ -3,5 +3,6 @@ package main
 // App struct to hold application wide dependencies and configuration
 // settings for our web application.
 type App struct {
-	HTMLDIR string
+	HTMLDir   string
+	StaticDir string
 }
