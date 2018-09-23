0e1b1deff2795c4e00a8fa5296784f28b12060d4
mukulrawat1986 <mukulsrawat@gmail.com>
2018-09-23 23:44:59 +0530

    Write a comment

--- gopl.io/ch1/helloworld/main.go
+++ gopl.io/ch1/helloworld/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+// Hello functions greets the person whose name is passed as a string. If no name
+// is provided the function prints out "Hello, World!"
 func Hello(w io.Writer, name string) {
 	if len(name) == 0 {
 		name = "World"
