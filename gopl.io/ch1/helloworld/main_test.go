23767c408eb0060f5ebfdf9ee13f9383ddf6f643
mukulrawat1986 <mukulsrawat@gmail.com>
2018-09-23 23:16:30 +0530

    We write the test function for the Hello function.
    Hello function prints out a greeting for the name of the person passed,
    and prints out a "Hello, World!", when no name is passed. The Hello
    function has a dependency of printing, so we solve this problem by
    Dependency Injection. We don't care about where the printing happens so
    in our test function we pass an interface to the Hello function, which
    will store whatever printing happens.

---
+++ gopl.io/ch1/helloworld/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+
+	// There is a dependency of printing in Hello function. So to capture that, we pass
+	// an interface, thus we can capture the output that is printed there. In our main function
+	// we pass the stdout
+	buffer := &bytes.Buffer{}
+
+	Hello(buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris!\n"
+
+	if got != want {
+		t.Errorf("wanted '%#v', got '%#v'", want, got)
+	}
+}
