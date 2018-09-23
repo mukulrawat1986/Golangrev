2b79a5f957f6d613b553cc4fda5ebdb7f2fdbe08
mukulrawat1986 <mukulsrawat@gmail.com>
2018-09-23 23:24:17 +0530

    Convert our test into a subtest for the case when a name is passed to the Hello function

--- gopl.io/ch1/helloworld/main_test.go
+++ gopl.io/ch1/helloworld/main_test.go
@@ -7,17 +7,19 @@ import (
 
 func TestHello(t *testing.T) {
 
-	// There is a dependency of printing in Hello function. So to capture that, we pass
-	// an interface, thus we can capture the output that is printed there. In our main function
-	// we pass the stdout
-	buffer := &bytes.Buffer{}
+	t.Run("when we pass a name to the Hello function", func(t *testing.T) {
+		// There is a dependency of printing in Hello function. So to capture that, we pass
+		// an interface, thus we can capture the output that is printed there. In our main function
+		// we pass the stdout
+		buffer := &bytes.Buffer{}
 
-	Hello(buffer, "Chris")
+		Hello(buffer, "Chris")
 
-	got := buffer.String()
-	want := "Hello, Chris!\n"
+		got := buffer.String()
+		want := "Hello, Chris!\n"
 
-	if got != want {
-		t.Errorf("wanted '%#v', got '%#v'", want, got)
-	}
+		if got != want {
+			t.Errorf("wanted '%#v', got '%#v'", want, got)
+		}
+	})
 }
