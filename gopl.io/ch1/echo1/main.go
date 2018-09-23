97a87cb7ae0cdcd998c4c07d35506251e51ef464
mukulrawat1986 <mukulsrawat@gmail.com>
2018-09-23 23:51:37 +0530

    Loop over the arguments and concatenate them in a string

--- gopl.io/ch1/echo1/main.go
+++ gopl.io/ch1/echo1/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // Echo prints its command line arguments
 func Echo(w io.Writer, args []string) {
+	var s, sep string
+	for i := 1; i < len(args); i++ {
+		s += sep + args[i]
+		sep = " "
+	}
+	fmt.Fprintln(w, s)
 }
 
 func main() {
