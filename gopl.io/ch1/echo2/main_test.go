299582bd30e45ce0a682e0e55cb0fd01e5ddac87
mukulrawat1986 <mukulsrawat@gmail.com>
2018-09-24 11:14:41 +0530

    Write tests for the Echo function.

---
+++ gopl.io/ch1/echo2/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestEcho(t *testing.T) {
+
+	// There are two dependecies in the Echo function. First the command line arguments
+	// and secondly the dependency of printing. We will pass a slice of strings to mock the
+	// command line arguments, and pass an interface to record what is being printed.
+	args := []string{"filename", "Hello", "World"}
+
+	buffer := &bytes.Buffer{}
+
+	Echo(bufffer, args)
+
+	got := buffer.String()
+	want := "Hello World\n"
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
