3955c96b6fe48387bfc6e6761053f435afa49218
mukulrawat1986 <mukulsrawat@gmail.com>
2018-09-24 11:25:09 +0530

    Finish the Echo function and pass the tests

--- gopl.io/ch1/echo2/main.go
+++ gopl.io/ch1/echo2/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 // Echo function prints its command line arguments
 func Echo(w io.Writer, args []string) {
+	s, sep := "", ""
+	for _, arg := range args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Fprintln(w, s)
 }
 
 func main() {
