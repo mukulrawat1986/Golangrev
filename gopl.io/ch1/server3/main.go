33922806d04a6ce2dc3c38435568a36c8870d9ad
mukulrawat1986 <mukulsrawat@gmail.com>
2018-09-24 19:46:00 +0530

    server3 echoes the HTTP request

---
+++ gopl.io/ch1/server3/main.go
@@ -0,0 +1,31 @@
+// server3 echoes the HTTP request
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	log.Println("Listening.....")
+	log.Fatal(http.ListenAndServe(":8000", mux))
+}
+
+// handler echoes the HTTP request
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Println(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
