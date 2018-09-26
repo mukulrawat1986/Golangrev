c140714e5adebef21002dfb0872aceb34077971a
mukulrawat1986 <mukulsrawat@gmail.com>
2018-09-26 14:49:11 +0530

    updated the constant name

--- gopl.io/ch2/boiling/main.go
+++ gopl.io/ch2/boiling/main.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 )
 
-const boiling = 212.0
+const boilingF = 212.0
 
 func main() {
-	var f = boiling
+	var f = boilingF
 	var c = (f - 32) * 5 / 9
 	fmt.Printf("boiling point = %g F or %g C\n", f, c)
 }
