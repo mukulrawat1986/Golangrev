e32fcd3a52d3cd60186adc213571c4daeebccc24
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-26 15:23:37 +0530

    Using HandlerFunc to make a handler and using closure

--- snippetbox.org/main.go
+++ snippetbox.org/main.go
@@ -6,29 +6,19 @@ import (
 	"time"
 )
 
-type timeHandler struct {
-	format string
-}
-
-func (th *timeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	tm := time.Now().Format(th.format)
-	w.Write([]byte("The time is: " + tm))
+func timeHandler(format string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tm := time.Now().Format(format)
+		w.Write([]byte("The time is: " + tm))
+	}
 }
 
 func main() {
 
 	mux := http.NewServeMux()
 
-	th1123 := &timeHandler{
-		format: time.RFC1123,
-	}
-
-	th3339 := &timeHandler{
-		format: time.RFC3339,
-	}
-
-	mux.Handle("/time/rfc1123", th1123)
-	mux.Handle("/time/rfc3339", th3339)
+	mux.Handle("/time/rfc1123", timeHandler(time.RFC1123))
+	mux.Handle("/time/rfc3339", timeHandler(time.RFC3339))
 
 	log.Println("Listening.....")
 	http.ListenAndServe(":3000", mux)
