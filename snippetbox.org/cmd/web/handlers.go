0459d781eacbc32ee028bfc36501c1a0c7962913
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-28 12:50:35 +0530

    update our handler to handle query strings in the url

--- snippetbox.org/cmd/web/handlers.go
+++ snippetbox.org/cmd/web/handlers.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -11,9 +15,19 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func ShowSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet...."))
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	// if id < 1 or couldn't be converted to an integer return a 404
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet (ID %d)\n", id)
 }
 
-func NewSnippet(w http.ResponseWriter, r *http.Request) {
+func NewSnippet(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte("Display a new snippet form...."))
 }
