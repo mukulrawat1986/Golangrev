882d6f358947d7648aa589760deaf8f05b37328f
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-28 14:29:44 +0530

    Add static files and serve static files using http.FileServer

--- snippetbox.org/cmd/web/main.go
+++ snippetbox.org/cmd/web/main.go
@@ -11,6 +11,14 @@ func main() {
 	mux.HandleFunc("/snippet", ShowSnippet)
 	mux.HandleFunc("/snippet/new", NewSnippet)
 
+	// create a file server which serves files out of the "./ui/static" directory
+	// The path given to http.Dir is relative to our project repository root
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// register fileServer as handler for all URL paths that start with "/static/"
+	// For matching path, we strip "/static" prefix before the request reaches the server
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
