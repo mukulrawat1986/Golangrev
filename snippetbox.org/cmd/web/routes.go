a23fdaac199955df3a8ce8bf3f4a65a711e5db62
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-29 14:21:19 +0530

    move our routes declaration in routes.go

--- snippetbox.org/cmd/web/routes.go
+++ snippetbox.org/cmd/web/routes.go
@@ -1,1 +1,18 @@
 package main
+
+import "net/http"
+
+func (app *App) Routes() *http.ServeMux {
+	// Declare a ServeMux and define the routes
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.Home)
+	mux.HandleFunc("/snippet", app.ShowSnippet)
+	mux.HandleFunc("/snippet/new", app.NewSnippet)
+
+	// use app.StaticDir field as location of the static file directory
+	fileServer := http.FileServer(http.Dir(app.StaticDir))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// return the serve mux
+	return mux
+}
