9d7421adbe21166392af3c497bd9adab9011fb91
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-29 12:05:38 +0530

    update our handlers to use the helper methods

--- snippetbox.org/cmd/web/handlers.go
+++ snippetbox.org/cmd/web/handlers.go
@@ -2,43 +2,18 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
-	"path/filepath"
 	"strconv"
 )
 
 // Home is a method against *App
 func (app *App) Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.NotFound(w)
 		return
 	}
 
-	// Because the Home handler function is now a method against App, it can access
-	// its fields. So we can build the paths to the HTML template files
-	files := []string{
-		filepath.Join(app.HTMLDir, "base.html"),
-		filepath.Join(app.HTMLDir, "home.page.html"),
-	}
-
-	// use template.ParseFiles() to create a template set
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-		return
-	}
-
-	// Use ExecuteTemplate() to execute the base template and write its content
-	// to http.ResponseWriter
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server error", 500)
-	}
-
+	app.RenderHTML(w, "home.page.html")
 }
 
 // ShowSnippet is a method against *App
@@ -49,7 +24,7 @@ func (app *App) ShowSnippet(w http.ResponseWriter, r *http.Request) {
 
 	// if id < 1 or couldn't be converted to an integer return a 404
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.NotFound(w)
 		return
 	}
 
