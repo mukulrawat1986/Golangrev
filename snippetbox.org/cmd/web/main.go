fb275c1630cc0bac4e5bbe05c1368c9e4a336a07
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-29 14:29:19 +0530

    remove routes from main, and use app.Routes()

--- snippetbox.org/cmd/web/main.go
+++ snippetbox.org/cmd/web/main.go
@@ -20,26 +20,11 @@ func main() {
 	// Initialize a new instance of App containing the dependencies
 	app := &App{
 		HTMLDir: *htmlDir,
+		StaticDir: *staticDir,
 	}
-
-	// use App object's methods as the handler functions
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.Home)
-	mux.HandleFunc("/snippet", app.ShowSnippet)
-	mux.HandleFunc("/snippet/new", app.NewSnippet)
-
-	// create a file server which serves files out of the "./ui/static" directory
-	// The path given to http.Dir is relative to our project repository root
-	// The value returned from the flag.String() function is a pointer to a string, so we need
-	// to derefernce it
-	fileServer := http.FileServer(http.Dir(*staticDir))
-
-	// register fileServer as handler for all URL paths that start with "/static/"
-	// For matching path, we strip "/static" prefix before the request reaches the server
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Again, we dereference the addr variable and use it as the network address
 	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.Routes())
 	log.Fatal(err)
 }
