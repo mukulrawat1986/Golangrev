7d182fc52b7d1f3d13a4598d495646ce45128d3d
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-29 11:55:50 +0530

    Centralized error handling, helpers to handler errors

---
+++ snippetbox.org/cmd/web/errors.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"runtime/debug"
+)
+
+// ServerError helper writes an error message and a stack trace to the log, then sends a generic
+// 500 Internal Server Error response to the user
+func (app *App) ServerError(w http.ResponseWriter, err error) {
+	log.Printf("%s\n%s", err.Error(), debug.Stack())
+	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+}
+
+// ClientError helper sends a specific status code and description to the user
+func (app *App) ClientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// NotFound helper is a wrapper around Client Error and returns 404 Not Found response to the User
+func (app *App) NotFound(w http.ResponseWriter, err error) {
+	app.ClientError(w, http.StatusNotFound)
+}
