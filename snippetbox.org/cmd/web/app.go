7a58398f410076ef0487446b987342b968b86a11
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-29 14:14:53 +0530

    We add the location of Static files as a dependency to the App object

--- snippetbox.org/cmd/web/app.go
+++ snippetbox.org/cmd/web/app.go
@@ -3,5 +3,6 @@ package main
 // Define an App struct to hold our application-wide dependencies and config
 // settings for our web application.
 type App struct {
-	HTMLDir string
+	HTMLDir   string
+	StaticDir string
 }
